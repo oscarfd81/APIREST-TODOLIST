4e0eb9af0e1cfb458973fd6dcffd908e0afd8397
Oscar <fernandezdiazoscar@gmail.com>
2026-05-20 00:54:17 +0200

    CARPETAS INNECESARIAS BORRADAS

--- todo-rest/todo-rest/src/main/java/com/oscar/todo_rest/tag/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oscar.todo_rest.tag;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-    Optional<Tag> findByName(String Name);
-}
